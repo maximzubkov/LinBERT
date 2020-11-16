c62c3341fcd275179687a593176813a3f89296c1
maximzubkov <zubkov.md@phystech.edu>
2020-11-17 01:42:18 +0300

    Add yelp dataset

---
+++ datasets/__init__.py
@@ -0,0 +1,3 @@
+from .classification_dataset import ClassificationDataset # noqa
+from .dataset_configs import datasets # noqa
+from .load_dataset import load_dataset # noqa
--- scripts/dowload_espiranto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=./data
-if [ ! -d $DATA_DIR ]
-then
-  mkdir $DATA_DIR
-fi
-wget -c https://cdn-datasets.huggingface.co/EsperBERTo/data/oscar.eo.txt -P $DATA_DIR/
-head -100 data/oscar.eo.txt > data/oscar_small.eo.txt
