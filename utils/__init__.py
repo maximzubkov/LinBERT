b02f126e2b00b165c14818f92a74e7d7b107b139
maximzubkov <47659865+maximzubkov@users.noreply.github.com>
2020-11-17 14:57:21 +0300

    Metrics (#9)
    * Add .dockerignore
    
    * Add metrics computing
    
    * Minor fix in .dockerignore

---
+++ utils/__init__.py
@@ -0,0 +1,40 @@
+import random
+from os.path import join
+
+import numpy as np
+from transformers import BertTokenizerFast
+from transformers import EvalPrediction
+from transformers.trainer_utils import set_seed
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+from datasets import load_dataset, datasets
+
+data_path = "data"
+
+
+def get_dataset(dataset_name: str, experiment_name: str, type: str, tokenizer: BertTokenizerFast):
+    return load_dataset(
+        experiment_name=experiment_name,
+        dataset_config=datasets[dataset_name],
+        dataset_path=join(data_path, dataset_name, type + ".csv"),
+        tokenizer=tokenizer,
+    )
+
+
+def set_seed_(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    set_seed(seed)
+
+
+def compute_metrics(pred: EvalPrediction):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
