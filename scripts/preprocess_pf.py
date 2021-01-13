aaaa0a3c6964dc0b272412c8b5904c03cac8a2ec
maximzubkov <47659865+maximzubkov@users.noreply.github.com>
2021-01-13 22:30:09 +0300

    Add resizing (#33)
    * Add rescaling
    
    * Fix preprocess script path
    
    * Add shape parametrs to preprocessing script
    
    * Add quotes preprocessed images

--- scripts/preprocess_pf.py
+++ scripts/preprocess_pf.py
@@ -1,9 +1,11 @@
+import csv
 from argparse import ArgumentParser
 from os import listdir, mkdir
 from os.path import join, isfile, exists
 
 import numpy as np
 import pandas as pd
+from skimage.transform import resize
 from tqdm import tqdm
 
 data_path = "data"
@@ -15,7 +17,7 @@ def create_vocab(dataset_path: str):
         f.write("\n".join(tokens))
 
 
-def preprocess(dataset: str):
+def preprocess(dataset: str, shape: tuple = (100, 100)):
     dataset_path = join(data_path, dataset)
     create_vocab(dataset_path)
     for split_ in ["train", "test"]:
@@ -29,16 +31,20 @@ def preprocess(dataset: str):
             file_path = join(path, file)
             if isfile(file_path) and (file_path[-3:] == "npz"):
                 loaded = np.load(file_path)
-                batch = loaded["images"].reshape(-1, 300 * 300).astype("int")
-                texts = [" ".join([str(e) for e in img]) for img in batch]
                 df = pd.DataFrame()
-                df["text"] = texts
-                df["label"] = loaded["labels"].reshape(-1)
-                df.to_csv(join(csv_path, f"""{file.split(".")[0]}.csv"""), index=False)
+                df["text"] = [
+                    "\"" + " ".join([
+                        str(e) for e in resize(img, shape, preserve_range=True).reshape(-1).astype(int)
+                    ]) + "\"" for img in loaded["images"]
+                ]
+                df["label"] = ["\"" + str(label) + "\"" for label in loaded["labels"].reshape(-1)]
+                df.to_csv(join(csv_path, f"""{file.split(".")[0]}.csv"""), index=False, quoting=csv.QUOTE_NONE)
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument("--dataset")
+    arg_parser.add_argument("--x_shape", type=int)
+    arg_parser.add_argument("--y_shape", type=int)
     args = arg_parser.parse_args()
-    preprocess(dataset=args.dataset)
+    preprocess(dataset=args.dataset, shape=(args.x_shape, args.y_shape))
