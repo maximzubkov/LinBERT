f398174334819a9b6ee0ad93d136bb1e7cabfd71
maximzubkov <zubkov.md@phystech.edu>
2021-01-31 14:17:29 +0300

    Refactor pos 2d embeddings

--- models/modules/positional_embedding.py
+++ models/modules/positional_embedding.py
@@ -1,45 +1,20 @@
-import torch
 import torch.nn as nn
 from transformers.modeling_bert import BertEmbeddings
+
+
+class Embedding2D(nn.Module):
+    def __init__(self, x_shape: int, y_shape: int, hidden_size: int):
+        super().__init__()
+        self.x_shape = x_shape
+        self.y_shape = y_shape
+        self.x_embeddings = nn.Embedding(self.x_shape, hidden_size)
+        self.y_embeddings = nn.Embedding(self.y_shape, hidden_size)
+
+    def forward(self, position_ids):
+        return self.y_embeddings(position_ids // self.x_shape) + self.y_embeddings(position_ids % self.y_shape)
 
 
 class Bert2DEmbeddings(BertEmbeddings):
     def __init__(self, config):
         super().__init__(config)
-        self.x_shape = config.x_shape
-        self.y_shape = config.y_shape
-
-        self.position_x_embeddings = nn.Embedding(self.x_shape, config.hidden_size // 2)
-        self.position_y_embeddings = nn.Embedding(self.y_shape, config.hidden_size // 2)
-        # position_ids (1, len position emb) is contiguous in memory and exported when serialized
-        self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
-        self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
-
-    def forward(
-        self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None, past_key_values_length=0
-    ):
-        if input_ids is not None:
-            input_shape = input_ids.size()
-        else:
-            input_shape = inputs_embeds.size()[:-1]
-
-        seq_length = input_shape[1]
-
-        if position_ids is None:
-            position_ids = self.position_ids[:, past_key_values_length: seq_length + past_key_values_length]
-
-        if token_type_ids is None:
-            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
-
-        if inputs_embeds is None:
-            inputs_embeds = self.word_embeddings(input_ids)
-        token_type_embeddings = self.token_type_embeddings(token_type_ids)
-
-        embeddings = inputs_embeds + token_type_embeddings
-        if self.position_embedding_type == "absolute":
-            position_x_embeddings = self.position_x_embeddings(position_ids % self.x_shape)
-            position_y_embeddings = self.position_y_embeddings(position_ids // self.y_shape)
-            embeddings += torch.cat([position_x_embeddings, position_y_embeddings], dim=-1)
-        embeddings = self.LayerNorm(embeddings)
-        embeddings = self.dropout(embeddings)
-        return embeddings
+        self.position_embeddings = Embedding2D(config.x_shape, config.y_shape, config.hidden_size)
