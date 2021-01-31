019a4e5d4a77fb6f0978f871fd3bc16d47e9118a
maximzubkov <zubkov.md@phystech.edu>
2021-02-01 00:51:28 +0300

    Fix 2d embed

--- models/modules/positional_embedding.py
+++ models/modules/positional_embedding.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers.modeling_bert import BertEmbeddings
 
@@ -9,9 +10,16 @@ class Embedding2D(nn.Module):
         self.y_shape = y_shape
         self.x_embeddings = nn.Embedding(self.x_shape, hidden_size)
         self.y_embeddings = nn.Embedding(self.y_shape, hidden_size)
+        self.spec_embeddings = nn.Embedding(2, hidden_size)
 
     def forward(self, position_ids):
-        return self.y_embeddings(position_ids // self.x_shape) + self.y_embeddings(position_ids % self.y_shape)
+        img_pos_ids = position_ids[:, 1:-1] - 1
+        img_emb = self.y_embeddings(img_pos_ids // self.x_shape) + self.y_embeddings(img_pos_ids % self.y_shape)
+        return torch.cat([
+            self.spec_embeddings.weight[0].unsqueeze(0),
+            img_emb.squeeze(),
+            self.spec_embeddings.weight[1].unsqueeze(0)
+        ], dim=0).unsqueeze(0)
 
 
 class Bert2DEmbeddings(BertEmbeddings):
