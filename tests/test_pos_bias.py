cc4e6a8dd79fc1fc8f7c97e4f2312c8554eaf4c4
maximzubkov <zubkov.md@phystech.edu>
2021-02-04 23:54:22 +0300

    Add tests for pos bias 2d

--- tests/test_pos_bias.py
+++ tests/test_pos_bias.py
@@ -3,35 +3,41 @@ from transformers import BertConfig
 
 from models.modules import PositionalBias
 
-fft_config = BertConfig(
+seq_len = 4
+num_heads = 2
+batch_size = 4
+embed_dim = 2
+v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
+
+config1 = BertConfig(
     vocab_size=15,
-    max_position_embeddings=4,
+    max_position_embeddings=seq_len,
     hidden_size=4,
-    num_attention_heads=2,
+    num_attention_heads=num_heads,
     num_hidden_layers=2,
     type_vocab_size=1,
     has_pos_attention=True,
     has_batch_norm=False,
-    pos_bias_type="fft",
+    pos_bias_type=None,
     feature_map="elu"
 )
 
-naive_config = BertConfig(
+config2 = BertConfig(
     vocab_size=15,
-    max_position_embeddings=4,
+    max_position_embeddings=seq_len,
     hidden_size=4,
-    num_attention_heads=2,
+    num_attention_heads=num_heads,
     num_hidden_layers=2,
     type_vocab_size=1,
     has_pos_attention=True,
     has_batch_norm=False,
-    pos_bias_type="naive",
+    pos_bias_type=None,
     feature_map="elu"
 )
 
 
 @torch.no_grad()
-def test_pos_bias():
+def _test(naive_config: BertConfig, fft_config: BertConfig):
     fft_pos_bias = PositionalBias(fft_config)
     fft_pos_bias.eval()
 
@@ -40,14 +46,20 @@ def test_pos_bias():
 
     naive_pos_bias.w.data = fft_pos_bias.w.data
 
-    batch_size = 4
-    seq_len = fft_config.max_position_embeddings
-    num_heads = fft_config.num_attention_heads
-    embed_dim = int(fft_config.hidden_size / fft_config.num_attention_heads)
-    v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-
     ppb_fft, z_pb_fft = fft_pos_bias(v)
 
     ppb_orig, z_pb_orig = naive_pos_bias(v)
     assert torch.allclose(z_pb_orig, z_pb_fft, atol=1e-4), "Z not equal"
     assert torch.allclose(ppb_orig, ppb_fft, atol=1e-4), "PPB not equal"
+
+
+def test_pos_bias():
+    config1.pos_bias_type = "naive"
+    config2.pos_bias_type = "fft"
+    _test(config1, config2)
+
+
+def test_pos_bias_2d():
+    config1.pos_bias_type = "naive_2d"
+    config2.pos_bias_type = "fft_2d"
+    _test(config1, config2)
