ba71326363a517e7def1b218551f15b36f034f4c
maximzubkov <zubkov.md@phystech.edu>
2021-07-26 20:55:05 +0300

    Add script to run glue

---
+++ glue.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DATASET_NAME=mnli
+
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      echo "options:"
+      echo "-h, --help                     show brief help"
+      echo "-d, --dataset NAME             specify dataset name, available: codeforces, poj_104"
+      exit 0
+      ;;
+    -d|--dataset*)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
+        DATASET_NAME=$2
+        shift 2
+      else
+        echo "Specify dataset name"
+        exit 1
+      fi
+      ;;
+    *)
+      echo "something went wrong"
+      exit 1
+  esac
+done
+
+python glue.py \
+  --model_name_or_path bert-base-cased \
+  --task_name "$DATASET_NAME" \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir /tmp/"$DATASET_NAME"
